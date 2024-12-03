c3ba47e200a41a410c31a7dee8d94c687d78e90d
66Gramms <meszarosricsi01@gmail.com>
2024-12-03 17:50:04 +0100

    Slices

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import "./store";
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: { amount: 1000 } });
+console.log(store.getState());
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
